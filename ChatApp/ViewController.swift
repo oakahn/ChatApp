9e8c1f796010577e8390289937bfc6633ff1ff1c
ChayawatS <mjpoak@gmail.com>
2018-06-01 14:41:01 +0700

    Add Position tabBar

--- ChatApp/ViewController.swift
+++ ChatApp/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var listItem = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    var listItem = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
     @IBOutlet weak var viewBar: UIView!
     @IBOutlet weak var sendText: UITextField!
     @IBOutlet weak var sendButton: UIButton!
@@ -29,10 +29,14 @@ class ViewController: UIViewController {
     func setup() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        sendButton.addTarget(self, action: #selector(send), for: .touchUpInside)
     }
     
-    @IBAction func sendMessage(_ sender: Any) {
-        sendText.resignFirstResponder()
+    @objc func send() {
+        listItem.append(sendText.text ?? "")
+        sendText.text = ""
+        chatTableView.reloadData()
+        getLastChat()
     }
     
     @objc func keyboardWillShow(notification: NSNotification) {
@@ -40,23 +44,27 @@ class ViewController: UIViewController {
             return
         }
         
-        listChatView.frame =  CGRect(x: 0,
-                                     y: 0,
-                                     width: view.frame.width,
-                                     height: view.frame.height - keyboardSize.height - 200)
-        
         chatTableView.frame =  CGRect(x: 0,
-                                     y: 0,
-                                     width: view.frame.width,
-                                     height: view.frame.height - keyboardSize.height - 200)
-        
+                                      y: 0,
+                                      width: view.frame.width,
+                                      height: view.frame.height - keyboardSize.height - 70)
         viewBar.frame.origin.y = view.frame.maxY - keyboardSize.height - 50
+        getLastChat()
     }
     
     @objc func keyboardWillHide(notification: NSNotification) {
-        listChatView.frame =  CGRect(x: 0, y: 50, width: view.frame.width, height: view.frame.height)
-        chatTableView.frame =  CGRect(x: 0, y: 50, width: view.frame.width, height: view.frame.height)
+        chatTableView.frame =  CGRect(x: 0,
+                                      y: 0,
+                                      width: view.frame.width,
+                                      height: view.frame.height - 70)
         viewBar.frame.origin.y = view.frame.maxY - 50
+    }
+    
+    func getLastChat() {
+        let lastSectionIndex = chatTableView.numberOfSections - 1
+        let lastRowIndex = chatTableView.numberOfRows(inSection: lastSectionIndex) - 1
+        let pathToLastRow = NSIndexPath(row: lastRowIndex, section: lastSectionIndex)
+        chatTableView?.scrollToRow(at: pathToLastRow as IndexPath, at: UITableViewScrollPosition.none, animated: true)
     }
 }
 
@@ -69,9 +77,5 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = listItem[indexPath.row]
         return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        sendMessage((Any).self)
     }
 }
